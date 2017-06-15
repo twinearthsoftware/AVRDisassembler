99dbc811bd7dfd52812a19f3c024534b697ac365
Christophe Diericx <christophe@diericx.net>
2017-06-15 20:55:08 -0300

    Update casing for consistency.

--- Source/AVRDisassembler/AVRDisassembler/build-release.bat
+++ Source/AVRDisassembler/AVRDisassembler/build-release.bat
@@ -15,11 +15,11 @@ dotnet build -c release
 
 echo -- Publishing ...
 
-dotnet publish -c release -r win7-x64 -o bin/dist/%appname%-%version%-win-x64
-dotnet publish -c release -r ubuntu.16.04-x64 -o bin/dist/%appname%-%version%-ubuntu.16.04-x64
-dotnet publish -c release -r ubuntu.16.10-x64 -o bin/dist/%appname%-%version%-ubuntu.16.10-x64
-dotnet publish -c release -r centos.7-x64 -o bin/dist/%appname%-%version%-centos.7-x64
-dotnet publish -c release -r debian.8-x64 -o bin/dist/%appname%-%version%-debian.8-x64
+dotnet publish -c release -r win7-x64 -o bin/dist/%appname%-%version%-Windows-x64
+dotnet publish -c release -r ubuntu.16.04-x64 -o bin/dist/%appname%-%version%-Ubuntu.16.04-x64
+dotnet publish -c release -r ubuntu.16.10-x64 -o bin/dist/%appname%-%version%-Ubuntu.16.10-x64
+dotnet publish -c release -r centos.7-x64 -o bin/dist/%appname%-%version%-Centos.7-x64
+dotnet publish -c release -r debian.8-x64 -o bin/dist/%appname%-%version%-Debian.8-x64
 dotnet publish -c release -r osx.10.10-x64 -o bin/dist/%appname%-%version%-OSX-Yosemite-x64
 dotnet publish -c release -r osx.10.11-x64 -o bin/dist/%appname%-%version%-OSX-ElCapitan-x64
 dotnet publish -c release -r osx.10.12-x64 -o bin/dist/%appname%-%version%-OSX-Sierra-x64
@@ -28,11 +28,11 @@ cd bin
 cd dist
 
 echo -- Zipping ...
-7z a %appname%-%version%-win-x64.zip %appname%-%version%-win-x64
-7z a %appname%-%version%-ubuntu.16.04-x64.zip %appname%-%version%-ubuntu.16.04-x64
-7z a %appname%-%version%-ubuntu.16.10-x64.zip %appname%-%version%-ubuntu.16.10-x64
-7z a %appname%-%version%-centos.7-x64.zip %appname%-%version%-centos.7-x64
-7z a %appname%-%version%-debian.8-x64.zip %appname%-%version%-debian.8-x64
+7z a %appname%-%version%-Windows-x64.zip %appname%-%version%-Windows-x64
+7z a %appname%-%version%-Ubuntu.16.04-x64.zip %appname%-%version%-Ubuntu.16.04-x64
+7z a %appname%-%version%-Ubuntu.16.10-x64.zip %appname%-%version%-Ubuntu.16.10-x64
+7z a %appname%-%version%-Centos.7-x64.zip %appname%-%version%-Centos.7-x64
+7z a %appname%-%version%-Debian.8-x64.zip %appname%-%version%-Debian.8-x64
 7z a %appname%-%version%-OSX-Yosemite-x64.zip %appname%-%version%-OSX-Yosemite-x64
 7z a %appname%-%version%-OSX-ElCapitan-x64.zip %appname%-%version%-OSX-ElCapitan-x64
 7z a %appname%-%version%-OSX-Sierra-x64.zip %appname%-%version%-OSX-Sierra-x64
