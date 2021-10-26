53866d5dc4893c7700becf9d7a08771d30dc856c
Alex Mosky <mskyaxl@protonmail.com>
2021-10-26 22:25:50 +0200

    switch to dotnet core version to 3.1 to be able to build for ubuntu 20/21; add build instruction for ubuntu

--- Source/AVRDisassembler/AVRDisassembler/build-release.bat
+++ Source/AVRDisassembler/AVRDisassembler/build-release.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set appname=AVRDisassembler
-set version=0.2.0
+set version=0.2.1
 
 echo **** Building %appname% release %version% ****
 
@@ -16,8 +16,8 @@ dotnet build -c release
 echo -- Publishing ...
 
 dotnet publish -c release -r win7-x64 -o bin/dist/%appname%-%version%-Windows-x64
-dotnet publish -c release -r ubuntu.16.04-x64 -o bin/dist/%appname%-%version%-Ubuntu.16.04-x64
-dotnet publish -c release -r ubuntu.16.10-x64 -o bin/dist/%appname%-%version%-Ubuntu.16.10-x64
+dotnet publish -c release -r ubuntu.21.04-x64 -o bin/dist/%appname%-%version%-Ubuntu.21.04-x64
+dotnet publish -c release -r ubuntu.21.04-x64 -o bin/dist/%appname%-%version%-Ubuntu.21.04-x64
 dotnet publish -c release -r centos.7-x64 -o bin/dist/%appname%-%version%-Centos.7-x64
 dotnet publish -c release -r debian.8-x64 -o bin/dist/%appname%-%version%-Debian.8-x64
 dotnet publish -c release -r osx.10.10-x64 -o bin/dist/%appname%-%version%-OSX-Yosemite-x64
@@ -29,8 +29,8 @@ cd dist
 
 echo -- Zipping ...
 7z a %appname%-%version%-Windows-x64.zip %appname%-%version%-Windows-x64
-7z a %appname%-%version%-Ubuntu.16.04-x64.zip %appname%-%version%-Ubuntu.16.04-x64
-7z a %appname%-%version%-Ubuntu.16.10-x64.zip %appname%-%version%-Ubuntu.16.10-x64
+7z a %appname%-%version%-Ubuntu.21.04-x64.zip %appname%-%version%-Ubuntu.21.04x64
+7z a %appname%-%version%-Ubuntu.21.04-x64.zip %appname%-%version%-Ubuntu.21.04-x64
 7z a %appname%-%version%-Centos.7-x64.zip %appname%-%version%-Centos.7-x64
 7z a %appname%-%version%-Debian.8-x64.zip %appname%-%version%-Debian.8-x64
 7z a %appname%-%version%-OSX-Yosemite-x64.zip %appname%-%version%-OSX-Yosemite-x64
